60241dc0e38f1e790c7657166470ca0f8667d16b
Paula <paula.s.rives@gmail.com>
2018-10-16 01:22:37 -0300

    styles

---
+++ src/app/app.component_BACKUP_22181.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { IAlumno } from './ialumno';
+import { IDocente } from './idocente';
+import { ICurso } from './icurso';
+import { Estado } from './estado.enum';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'facultad-app';
+  alumno = alumno;
+  curso = curso;
+  docente = docente;
+
+  public cambiarEstadoCurso (value): void {
+    this.curso.estado = <Estado>value;
+  }                   
+      
+}
+
+var docente = <IDocente>{nombre: 'Fernando', 
+                        apellido: 'Arias'};
+var curso = <ICurso>{id: 22,
+                      titulo: "Angular",
+                      fechaInicio: 1551657600000,
+                      fechaFin: 1549238400000,
+                      asistentes: [
+                          "Jimena Sanchez",
+                          "Joaquín Ferreyra",
+                          "Adolfo Carrasco",
+                          "Juan Ignacio López",
+                          "Nadia de Rosa"
+                      ],
+                      profesor: [
+                          "Fernando Arias"
+                      ],
+                      laboratorio: 7,
+                      estado: <Estado>0} 
+var alumno = <IAlumno>{nombre: 'Paula',
+                      apellido: 'Rives', 
+                      cursos: [curso], };
+
+                 
+
+
